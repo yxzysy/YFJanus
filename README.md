ce2b00d9f8b36a855fc4478ae7656c880caae108
yxzysy <24765189@qq.com>
2025-02-01 23:31:22 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,2 @@
+A Pinokio version of Janus-Pro from DeepSeek, an advanced version of Janus, improving both multimodal understanding and visual generation significantly.
+The original model is available at [DeepSeek/Janus-Pro-7B](https://huggingface.co/DeepSeek/Janus-Pro-7B).
---
+++ install.js
@@ -0,0 +1,50 @@
+module.exports = {
+  run: [
+    // Edit this step to customize the git repository to use
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/deepseek-ai/Janus.git app",
+        ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
+        params: {
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          // xformers: true   // uncomment this line if your project requires xformers
+        }
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "uv pip install gradio devicetorch",
+          "uv pip install -r requirements.txt"
+        ]
+      }
+    },
+    {
+      method: "fs.link",
+      params: {
+        venv: "app/env"
+      }
+    },
+    {
+      method: "fs.copy",
+      params: {
+        src: "app/demo/app_januspro.py",
+        dest: "app/app_januspro.py"
+      }
+    }
+  ]
+}
